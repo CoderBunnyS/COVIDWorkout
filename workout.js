66a212f126da485aa08b793b300c15ab772667e2
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2021-01-28 10:48:23 -0800

    added button functionality. Can access workouts by time

--- workout.js
+++ workout.js
@@ -1,3 +1,7 @@
+yoga15 = ["https://www.youtube.com/embed/g_tea8ZNk5A", "https://www.youtube.com/embed/DYY8KovKO3M", "https://www.youtube.com/embed/JgOVqfthILw", "https://www.youtube.com/embed/CKsrfXux9IA", "https://www.youtube.com/embed/utrAlZf_Pjs"]
+yoga30 = ["https://www.youtube.com/embed/pavRUoAT1FQ", "https://www.youtube.com/embed/yyYlVOqgj0A", "https://www.youtube.com/embed/TBXffMRU8MM", "https://www.youtube.com/embed/mPYA6_rKdtU", "https://www.youtube.com/embed/5v1wqxnrmrk"]
+yoga45 = ["https://www.youtube.com/embed/GLy2rYHwUqY", "https://www.youtube.com/embed/X4MchDHkJWw", "https://www.youtube.com/embed/kV6HfIJiLeE", "https://www.youtube.com/embed/ALO4udhcXuk", "https://www.youtube.com/embed/j-PM39D5vrs"]
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function timeFunction() {
@@ -21,8 +25,13 @@ function typeFunction() {
     }
   }
 
-  function screen() {
-    
-    document.getElementById('ytVideo').src = "https://www.youtube.com/embed/bo2KQer1KNM";
+  function screen15() {   
+    document.getElementById('ytVideo').src = yoga15[Math.floor(Math.random() * yoga15.length)];
+  };
+  function screen30() {   
+    document.getElementById('ytVideo').src = yoga30[Math.floor(Math.random() * yoga30.length)];
+  };
+  function screen45() {   
+    document.getElementById('ytVideo').src = yoga45[Math.floor(Math.random() * yoga45.length)];
   };
   
